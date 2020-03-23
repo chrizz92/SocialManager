bc75adaed01be1552ed8532aecf93b461b4987d5
chrizz92 <christianschadler@gmx.at>
2020-03-23 22:53:55 +0100

    aktualisiert

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,12 +1,68 @@
-﻿using System;
+﻿using SocialManager;
+using System;
 
 namespace ConsoleApp
 {
     internal class Program
     {
+        /// <summary>
+        /// A private method that creates a new instances of the class pupil and calls the 5 Set-Methods of the created object.
+        /// </summary>
+        /// <returns>The created pupil object</returns>
+        private static Pupil CreateNewPupil()
+        {
+            Console.Clear();
+            Pupil pupil = new Pupil();
+            Console.Write("Bitte Vornamen eingeben:{0,19}", " ");
+            pupil.SetFirstName(Console.ReadLine());
+            Console.Write("Bitte Nachnamen eingeben:{0,18}", " ");
+            pupil.SetLastName(Console.ReadLine());
+            Console.Write("Bitte Geburtstag eingeben:{0,17}", " ");
+            pupil.SetDateOfBirth(Convert.ToDateTime(Console.ReadLine()));
+            Console.Write("Bitte Postleitzahl des Wohnortes eingeben:{0,1}", " ");
+            pupil.SetZipCode(Console.ReadLine());
+            Console.Write("Bitte Name des Wohnortes eingeben:{0,9}", " ");
+            pupil.SetCity(Console.ReadLine());
+            return pupil;
+        }
+
+        /// <summary>
+        /// A private method
+        /// </summary>
+        /// <returns></returns>
+        private static int SetSizeOfPupilArray()
+        {
+            Console.Write("Bitte maximale Anzahl der Schüler eingeben: ");
+            int arraySize = Convert.ToInt32(Console.ReadLine());
+            return arraySize;
+        }
+
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string firstNameToLookFor = "";
+            int maxPupils = SetSizeOfPupilArray();
+            Pupil[] pupils = new Pupil[maxPupils];
+
+            for (int i = 0; i < maxPupils; i++)
+            {
+                pupils[i] = CreateNewPupil();
+            }
+
+            do
+            {
+                Console.Write("Welcher Vorname ist gesucht <# zum Beenden>: ");
+                firstNameToLookFor = Console.ReadLine().Trim().ToLower();
+
+                for (int i = 0; i < maxPupils; i++)
+                {
+                    if (pupils[i].GetFirstName().ToLower() == firstNameToLookFor)
+                    {
+                        Console.WriteLine("{0}, {1} Jahre", pupils[i].GetFullName(), pupils[i].GetYearsOld());
+                    }
+                }
+                Console.Write("Bitte beliebige Taste zum Fortsetzen");
+                Console.ReadKey()
+            } while (firstNameToLookFor != "#");
         }
     }
 }
